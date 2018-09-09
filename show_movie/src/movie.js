4902709fca59211a510a3fa6bffa6f9bb6119cad
griffinGC <griffinDouble@gmail.com>
2018-09-09 10:45:59 +0900

    movie

--- show_movie/src/movie.js
+++ show_movie/src/movie.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import './movie.css';
 
 
-class movie extends Component{
-    
+class Movie extends Component{
+    render(){
+        return(
+            <div>
+                <img src="https://cdn.namuwikiusercontent.com/s/13785482ee9d3c792dc09eb2404f270a794c0f0435ce63473acf6cf2e34fc9e338fa812351e1710a73790fb8c3eb20970f07ad793f2d0c9e25468d510840efde911da5703036717866031fc581cdd710?e=1539639796&k=YIvwkeHAC0Hyd0Qb9GX1wA" width="150px"height="300px" alt="Matrix"/>
+                <h1>this is the movie</h1>
+            </div>
+        )
+    }
 }
+
+export default Movie
